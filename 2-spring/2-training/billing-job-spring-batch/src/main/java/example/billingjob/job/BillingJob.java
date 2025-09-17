28683c298edc981d761020c6386269d161f1029a
kevinkouomeu <kevinkouomeu@icloud.com>
2025-09-17 08:58:03 +0200

    Batch configuration with Job Params

--- 2-spring/2-training/billing-job-spring-batch/src/main/java/example/billingjob/job/BillingJob.java
+++ 2-spring/2-training/billing-job-spring-batch/src/main/java/example/billingjob/job/BillingJob.java
@@ -1,9 +1,22 @@
 package example.billingjob.job;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.repository.JobRepository;
 
 public class BillingJob implements Job {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingJob.class);
+    private final JobRepository jobRepository;
+
+    public BillingJob(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
 
     @Override
     public String getName() {
@@ -12,6 +25,14 @@ public class BillingJob implements Job {
 
     @Override
     public void execute(JobExecution execution) {
-        System.out.println("processing billing information");
+        JobParameters jobParameters = execution.getJobParameters();
+        String inputFile = jobParameters.getString("input.file");
+
+        log.info("processing billing information from file {}", inputFile);
+
+        execution.setStatus(BatchStatus.COMPLETED);
+        execution.setExitStatus(ExitStatus.COMPLETED);
+
+        this.jobRepository.update(execution);
     }
 }
